31d8eb2c0b8ca9ab9a69605535390e2c847f9acb
Luc-Veldhuis <jfveldhuis@outlook.com>
2017-02-15 12:39:11 +0100

    Add slides analysis 2b

--- js/data.js
+++ js/data.js
@@ -2,175 +2,181 @@
     {
         "slides": [
             {
-                "length": 15,
-                "location": "files/statistics/slides/slides8.pdf",
-                "slug": "slides8",
+                "length": 15, 
+                "location": "files/statistics/slides/slides8.pdf", 
+                "slug": "slides8", 
                 "title": "Slides 8"
-            },
+            }, 
             {
-                "length": 15,
-                "location": "files/statistics/slides/slides9.pdf",
-                "slug": "slides9",
+                "length": 15, 
+                "location": "files/statistics/slides/slides9.pdf", 
+                "slug": "slides9", 
                 "title": "Slides 9"
-            },
+            }, 
             {
-                "length": 17,
-                "location": "files/statistics/slides/slides10.pdf",
-                "slug": "slides10",
+                "length": 17, 
+                "location": "files/statistics/slides/slides10.pdf", 
+                "slug": "slides10", 
                 "title": "Slides 10"
-            },
+            }, 
             {
-                "length": 13,
-                "location": "files/statistics/slides/slides11.pdf",
-                "slug": "slides11",
+                "length": 13, 
+                "location": "files/statistics/slides/slides11.pdf", 
+                "slug": "slides11", 
                 "title": "Slides 11"
-            },
+            }, 
             {
-                "length": 15,
-                "location": "files/statistics/slides/slides12.pdf",
-                "slug": "slides12",
+                "length": 15, 
+                "location": "files/statistics/slides/slides12.pdf", 
+                "slug": "slides12", 
                 "title": "Slides 12"
-            },
+            }, 
             {
-                "length": 8,
-                "location": "files/statistics/slides/slides13.pdf",
-                "slug": "slides13",
+                "length": 8, 
+                "location": "files/statistics/slides/slides13.pdf", 
+                "slug": "slides13", 
                 "title": "Slides 13"
             }
-        ],
-        "slug": "statistics",
+        ], 
+        "slug": "statistics", 
         "title": "Statistics"
-    },
+    }, 
     {
         "slides": [
             {
-                "length": 6,
-                "location": "files/logic/slides/slides1.pdf",
-                "slug": "slides1",
+                "length": 6, 
+                "location": "files/logic/slides/slides1.pdf", 
+                "slug": "slides1", 
                 "title": "Slides 1"
             }
-        ],
-        "slug": "logic",
+        ], 
+        "slug": "logic", 
         "title": "Logic"
-    },
+    }, 
     {
         "slides": [
             {
-                "length": 23,
-                "location": "files/analysis1b/slides/slides1.pdf",
-                "slug": "slides1",
+                "length": 23, 
+                "location": "files/analysis1b/slides/slides1.pdf", 
+                "slug": "slides1", 
                 "title": "Slides 1"
-            },
+            }, 
             {
-                "length": 17,
-                "location": "files/analysis1b/slides/slides2.pdf",
-                "slug": "slides2",
+                "length": 17, 
+                "location": "files/analysis1b/slides/slides2.pdf", 
+                "slug": "slides2", 
                 "title": "Slides 2"
-            },
+            }, 
             {
-                "length": 15,
-                "location": "files/analysis1b/slides/slides3.pdf",
-                "slug": "slides3",
+                "length": 15, 
+                "location": "files/analysis1b/slides/slides3.pdf", 
+                "slug": "slides3", 
                 "title": "Slides 3"
-            },
+            }, 
             {
-                "length": 16,
-                "location": "files/analysis1b/slides/slides4.pdf",
-                "slug": "slides4",
+                "length": 16, 
+                "location": "files/analysis1b/slides/slides4.pdf", 
+                "slug": "slides4", 
                 "title": "Slides 4"
-            },
+            }, 
             {
-                "length": 12,
-                "location": "files/analysis1b/slides/slides5.pdf",
-                "slug": "slides5",
+                "length": 12, 
+                "location": "files/analysis1b/slides/slides5.pdf", 
+                "slug": "slides5", 
                 "title": "Slides 5"
-            },
+            }, 
             {
-                "length": 10,
-                "location": "files/analysis1b/slides/slides6.pdf",
-                "slug": "slides6",
+                "length": 10, 
+                "location": "files/analysis1b/slides/slides6.pdf", 
+                "slug": "slides6", 
                 "title": "Slides 6"
-            },
+            }, 
             {
-                "length": 12,
-                "location": "files/analysis1b/slides/slides7.pdf",
-                "slug": "slides7",
+                "length": 12, 
+                "location": "files/analysis1b/slides/slides7.pdf", 
+                "slug": "slides7", 
                 "title": "Slides 7"
-            },
+            }, 
             {
-                "length": 12,
-                "location": "files/analysis1b/slides/slides8.pdf",
-                "slug": "slides8",
+                "length": 12, 
+                "location": "files/analysis1b/slides/slides8.pdf", 
+                "slug": "slides8", 
                 "title": "Slides 8"
-            },
+            }, 
             {
-                "length": 11,
-                "location": "files/analysis1b/slides/slides9.pdf",
-                "slug": "slides9",
+                "length": 11, 
+                "location": "files/analysis1b/slides/slides9.pdf", 
+                "slug": "slides9", 
                 "title": "Slides 9"
-            },
+            }, 
             {
-                "length": 10,
-                "location": "files/analysis1b/slides/slides10.pdf",
-                "slug": "slides10",
+                "length": 10, 
+                "location": "files/analysis1b/slides/slides10.pdf", 
+                "slug": "slides10", 
                 "title": "Slides 10"
-            },
+            }, 
             {
-                "length": 11,
-                "location": "files/analysis1b/slides/slides11.pdf",
-                "slug": "slides11",
+                "length": 11, 
+                "location": "files/analysis1b/slides/slides11.pdf", 
+                "slug": "slides11", 
                 "title": "Slides 11"
-            },
+            }, 
             {
-                "length": 16,
-                "location": "files/analysis1b/slides/slides12.pdf",
-                "slug": "slides12",
+                "length": 16, 
+                "location": "files/analysis1b/slides/slides12.pdf", 
+                "slug": "slides12", 
                 "title": "Slides 12"
-            },
+            }, 
             {
-                "length": 12,
-                "location": "files/analysis1b/slides/slides13.pdf",
-                "slug": "slides13",
+                "length": 12, 
+                "location": "files/analysis1b/slides/slides13.pdf", 
+                "slug": "slides13", 
                 "title": "Slides 13"
             }
-        ],
-        "slug": "analysis1b",
+        ], 
+        "slug": "analysis1b", 
         "title": "Analysis 1B"
-    },
+    }, 
     {
         "slides": [
             {
-                "length": 15,
-                "location": "files/analysis2a/slides/slides1.pdf",
-                "slug": "slides1",
+                "length": 15, 
+                "location": "files/analysis2a/slides/slides1.pdf", 
+                "slug": "slides1", 
                 "title": "Slides 1"
-            },
+            }, 
             {
-                "length": 13,
-                "location": "files/analysis2a/slides/slides2.pdf",
-                "slug": "slides2",
+                "length": 13, 
+                "location": "files/analysis2a/slides/slides2.pdf", 
+                "slug": "slides2", 
                 "title": "Slides 2"
+            }, 
+            {
+                "length": 21, 
+                "location": "files/analysis2a/slides/slides3.pdf", 
+                "slug": "slides3", 
+                "title": "Slides 3"
             }
-        ],
-        "slug": "analysis2a",
+        ], 
+        "slug": "analysis2a", 
         "title": "Analyse 2A"
-    },
+    }, 
     {
         "slides": [
             {
-                "length": 11,
-                "location": "files/groepenTheorie/slides/slides1.pdf",
-                "slug": "slides1",
+                "length": 11, 
+                "location": "files/groepenTheorie/slides/slides1.pdf", 
+                "slug": "slides1", 
                 "title": "Slides 1"
-            },
+            }, 
             {
-                "length": 13,
-                "location": "files/groepenTheorie/slides/slides2.pdf",
-                "slug": "slides2",
+                "length": 13, 
+                "location": "files/groepenTheorie/slides/slides2.pdf", 
+                "slug": "slides2", 
                 "title": "Slides 2"
             }
-        ],
-        "slug": "groepenTheorie",
+        ], 
+        "slug": "groepenTheorie", 
         "title": "Groepen Theorie"
     }
 ]
